8b70bbbce79675472bca836e89d6109c2b3eeae1
william-if <williamcnh1@gmail.com>
2026-09-08 18:44:33 -0300

    Finished version one

--- Python Codes/test_cmd.py
+++ Python Codes/test_cmd.py
@@ -2,11 +2,18 @@ from topology.run_topology import run_topology
 import numpy as np
 
 run_topology(
-    topology='E1',
-    l_max=30,
-    Lx=1.0, Ly=1.0, Lz=1.0,
-    beta=90, alpha=90,
-    x0 = np.array([0.1, 0.2, 0.3]),
-    do_polarization=False,
-    normalize=True,
-)
+        topology='E1',
+        l_max=5,
+        Lx=1.0,
+        Ly=1.0,
+        Lz=1.0,
+        beta=90,
+        alpha=90,
+        gamma=0,
+        do_polarization=False,
+        normalize=True,
+        l_range=np.array([[2, 5]]),
+        lp_range=np.array([[2, 5]])
+    )
+
+print("Example 1 completed.\n")
